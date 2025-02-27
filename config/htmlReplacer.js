7176da1a3b8e165be5511c3b3121a2e575030864
Ivan Mozheev <mau0hezzz@hotmail.com>
2025-02-27 17:47:27 +0300

    paths fix

--- config/htmlReplacer.js
+++ config/htmlReplacer.js
@@ -5,27 +5,19 @@ import path from "path";
 export async function htmlReplacer() {
   let config;
 
+  const transformIndexHtml = (html) => {
+    return html.replaceAll('/assets', 'assets');
+  }
+
   return {
     name: 'htmlReplacer',
     enforce: 'post',
     configResolved(resolvedConfig) {
         config = resolvedConfig;
     },
-    transform() {
-        return { code: transformIndexHtml(), map: undefined };
+    transform(source) {
+        return { code: transformIndexHtml(source), map: undefined };
     },
-    transformIndexHtml() {
-      const htmlDir = path.resolve(__dirname, "../dist");
-      const files = fs.readdirSync(htmlDir);
-    
-      for (const file of files) {
-        if (file.endsWith(".html")) {
-          const filePath = path.join(htmlDir, file);
-          let html = fs.readFileSync(filePath, "utf-8");
-          
-          return html.replaceAll('/assets', 'assets');
-        }
-      }
-    }
+    transformIndexHtml
   }
 }

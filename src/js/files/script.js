160775d2d11fc160bd96494f42d452aeb680463d
Ivan Mozheev <mau0hezzz@hotmail.com>
2025-03-03 21:49:21 +0300

    fixes #1

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,5 +4,39 @@ import { debounce, isMobile } from "./functions.js";
 import { mhzModules } from "./modules.js";
 import { gotoBlock } from "./scroll/gotoblock.js";
 
+const mmd1 = matchMedia('(min-width: 1920px)');
 
-// gotoBlock('.wrapper', false, 0)
+document.addEventListener('DOMContentLoaded', ()=>{
+  if (mmd1.matches) {
+    setSvgWH();
+  }
+})
+
+
+function setSvgWH() {
+  const svgs = document.querySelectorAll('svg');
+  if (!svgs.length) return
+
+  svgs.forEach(svg=>{
+    const width = svg.getAttribute('width') || svg.offsetWidth;
+    const height = svg.getAttribute('height') || svg.offsetHeight;
+
+    const widthRem = width / 16;
+    const heightRem = height / 16;
+
+    let checked = true;
+    let selectorsArr = ['.header__logo'];
+
+    selectorsArr.forEach(selector=>{
+      if (svg.closest(selector)) {
+        checked = false;
+      }
+    })
+
+    if (checked) {
+      svg.style.setProperty('min-width', `${widthRem}rem`)
+      // svg.style.setProperty('flex-basis', `${widthRem}rem`)
+      svg.style.setProperty('min-height', `${heightRem}rem`)
+    }
+  })
+}
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -4,6 +4,7 @@ import {Splide} from '@splidejs/splide';
 
 // or only core styles
 import '@splidejs/splide/css/core';
+import { debounce } from './functions';
 
 
 
@@ -32,6 +33,12 @@ function initSliders() {
       } );
     }
 
+    if (navigator.userAgent.match(/Mac OS/i)) {
+      splide.root.addEventListener('mousewheel', (e)=>{
+        onWheel(e, splide);
+      })
+    }
+
     splide.mount();
   }
   if (document.querySelector('.assort__slider')) {
@@ -49,19 +56,43 @@ function initSliders() {
 
     if (bar) {
       splide.on( 'mounted move', function () {
-        let end  = splide.Components.Controller.getEnd() + 1;
-        let rate = Math.min( ( splide.index + 1 ) / end, 1 );
-        let width = 100 / end;
-        let translate = splide.Components.Controller.getIndex() * 100;
-        bar.style.width = String( width ) + '%';
-        bar.style.translate =`${translate}% 0%`;
+        // let end  = splide.Components.Controller.getEnd() + 1;
+        // let rate = Math.min( ( splide.index + 1 ) / end, 1 );
+        // let width = 100 / end;
+        // let translate = splide.Components.Controller.getIndex() * 100;
+        // bar.style.width = String( width ) + '%';
+        // bar.style.translate =`${translate}% 0%`;
+
+        
+        var end  = splide.Components.Controller.getEnd() + 1;
+        var rate = Math.min( ( splide.index + 1 ) / end, 1 );
+        bar.style.width = String( 100 * rate ) + '%';
       } );
+    }
+
+    if (navigator.userAgent.match(/Mac OS/i)) {
+      splide.root.addEventListener('mousewheel', (e)=>{
+        onWheel(e, splide);
+      })
     }
 
     splide.mount();
   }
 }
 
+function onWheel(e, splide) {
+  if (Math.abs(e.deltaX) > 1) {
+    e.preventDefault();
+    console.log(e.deltaX);
+    let coef = e.deltaX > 0 ? 1 : -1;
+    let dest = splide.Components.Controller.getIndex() + coef;
+    const end = splide.Components.Controller.getEnd();
+
+    if (dest >= 0 && dest <= end) {
+      splide.Components.Controller.go(dest)
+    }
+  }
+}
 
 window.addEventListener("load", function (e) {
 	initSliders();
